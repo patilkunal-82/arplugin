ee270a54535f51f6a5578476d47dfa2cae017d14
Kunal <patilkunal@gmail.com>
2021-07-13 16:55:17 -0700

    changed the name of the plugin

--- src/ios/ArPlugin.swift
+++ src/ios/ArPlugin.swift
@@ -34,7 +34,7 @@ import Contacts
     @objc(getContacts:) func getContacts(_ command CDVInvokedUrlCommand) {
 
           var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-          let value = command.getString("filter") ?? ""
+          
           let contactStore = CNContactStore();
           var contacts = [Any]()
           let keys = [
@@ -47,7 +47,7 @@ import Contacts
 
             if let error = error {
                 print("failed to request access", error)
-                command.reject("aaccess denied")
+                
                 return
             }
 
@@ -64,22 +64,20 @@ import Contacts
                        }
                        print(contacts)
                         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: contacts)
-                       command.success([
-                           "results": contacts
-                       ])
+                       
                       
 
                 } catch {
                        print("unable to fetch contacts")
                        pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something wrong")
-                       command.reject("Unable to fetch contacts")
+                       
                        
                 }
 
             } else {
                     print("access denied")
                       pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something wrong")
-                    command.reject("access denied")
+                    
                   
             }
 
@@ -92,55 +90,3 @@ import Contacts
 
 }
 
-/* 
-@objc(ContactPluginPlugin)
-public class ContactPluginPlugin: CAPPlugin {
-    private let implementation = ContactPlugin()
-
-    
-    @objc func getContacts(_ call: CAPPluginCall) {
-            
-        let value = call.getString("filter") ?? ""
-            // You could filter based on the value passed to the function!
-            
-            let contactStore = CNContactStore()
-            var contacts = [Any]()
-            let keys = [
-                    CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
-                            CNContactPhoneNumbersKey,
-                            CNContactEmailAddressesKey
-                    ] as [Any]
-            let request = CNContactFetchRequest(keysToFetch: keys as! [CNKeyDescriptor])
-            
-            contactStore.requestAccess(for: .contacts) { (granted, error) in
-                if let error = error {
-                    print("failed to request access", error)
-                    call.reject("access denied")
-                    return
-                }
-                if granted {
-                   do {
-                       try contactStore.enumerateContacts(with: request){
-                               (contact, stop) in
-                        contacts.append([
-                            "firstName": contact.givenName,
-                            "lastName": contact.familyName,
-                            "telephone": contact.phoneNumbers.first?.value.stringValue ?? ""
-                        ])
-                       }
-                       print(contacts)
-                       call.success([
-                           "results": contacts
-                       ])
-                   } catch {
-                       print("unable to fetch contacts")
-                       call.reject("Unable to fetch contacts")
-                   }
-                } else {
-                    print("access denied")
-                    call.reject("access denied")
-                }
-            }
-        }
-}
-*/
